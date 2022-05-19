ff37f1c6ca5bf0a3864d16f47b49517f52e98dde
x09087on <56896132+x09087on@users.noreply.github.com>
2022-05-19 14:19:37 +0200

    Update admin.controller.ts

--- Backend/api/events/admin.controller.ts
+++ Backend/api/events/admin.controller.ts
@@ -3,22 +3,47 @@ import { inject, injectable } from 'inversify';
 import { controller, httpGet, httpPost, httpPut, interfaces, requestParam } from 'inversify-express-utils';
 import { DatabaseService } from '../../core/services/database.service';
 import { LoggerService } from '../../core/services/logger.service';
+import { PasswordPolicyEnforcerService } from '../../core/services/password-policy-enforcer.service';
 
 @controller('/admin')
 @injectable()
 export class AdminController implements interfaces.Controller {
   constructor(
-    @inject(DatabaseService.name) private databaseService: DatabaseService, @inject(LoggerService.name) private loggerService: LoggerService
+    @inject(DatabaseService.name) private databaseService: DatabaseService, @inject(LoggerService.name) private loggerService: LoggerService, @inject(PasswordPolicyEnforcerService.name) private passwordPolicyEnforcerService
   ) {}
 
-  @httpPost('/:firstname&:lastname&:email&:username&:password')
-  public createAdmin(request: Request, response: Response): void {
-    this.databaseService.createAdmin(request.params.firstname, request.params.lastname, request.params.email, request.params.username, request.params.password)
-    .then((result) => {
-      this.loggerService.info('admin registered');
-      this.databaseService.createHistoryEntry(request.params.username, 'new admin created');
-      response.json(result);
-    });
+  @httpPost('/:firstname&:lastname&:email&:username&:password&:passwordrepetition')
+  public createAdmin(request: Request, response: Response): void 
+  {
+    let password = request.params.password;
+    let passwortRepetition = request.params.passwordrepetition;
+
+    if (password != passwortRepetition)
+    {
+      this.loggerService.info('Error: The password and the password repetition do not match!');
+      
+      let result = { errors: 1,
+                     errorSource: "Password and Password Repetition.",
+                     message: 'Error: The password and the password repetition do not match!' };
+    }
+    else if (this.passwordPolicyEnforcerService.enforcePasswordPolicies(password))
+    {
+      this.databaseService.createAdmin(request.params.firstname, 
+                                       request.params.lastname, 
+                                       request.params.email, 
+                                       request.params.username, 
+                                       request.params.password).then((result) => {
+        this.loggerService.info('New Admin registered!');
+        this.databaseService.createHistoryEntry(request.params.username, 'New Admin registered!');
+        response.json(result);
+      });
+    }
+    else {
+      this.loggerService.info('Error: Password doesnt match policies. Length must be greater 16. Must containe atleast one number, one uppercase and one symbol from ["!", "?", ",", ".", ":", "-"]!');
+      let result = { errors: 1,
+                     errorSource: "Password",
+                     message: 'Error: Password doesnt match policies. Length must be greater 16. Must containe atleast one number, one uppercase and one symbol from ["!", "?", ",", ".", ":", "-"]!'};
+    }
   }
 
   @httpPut('/:username&:password&:firstname&:lastname&:email&:newusername&:newpassword')
